d9f6c998c60dc31b3cf4e45b8e865c04665a666c
siri191 <164751340+siri191@users.noreply.github.com>
2024-09-24 10:07:50 +0530

    Update http_1.js

--- httpModules/http_1.js
+++ httpModules/http_1.js
@@ -1,13 +1,24 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const server = http.createServer((req, res)=>{
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, 'reg.html');
+    
 
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.write("<center>Welcome to Node HTTP module</center>")
-   
-    res.end()
-})
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write("<center>404 - File Not Found</center>");
+            res.end();
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        }
+    });
+});
 
-server.listen(9000, ()=>{
-    console.log("Server is running @ http://localhost:9000")
-})
+server.listen(9000, () => {
+    console.log("Server is running @ http://localhost:9000");
+});
